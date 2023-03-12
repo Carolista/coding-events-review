ab8a3d96d1c366adfb3997316c5be14233c4bff1
Carolista <cjones.renaissancewoman@gmail.com>
2023-03-12 12:14:47 -0500

    added create events form with controllers; added initial fragments

--- src/main/java/org/LaunchCode/codingeventsreview/controllers/EventController.java
+++ src/main/java/org/LaunchCode/codingeventsreview/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.LaunchCode.codingeventsreview.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +13,26 @@ import java.util.List;
 @Controller
 @RequestMapping("/events")
 public class EventController {
+    private static List<String> events = new ArrayList<>();
 
+    // Renders http://localhost:8080/events
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code with Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
+    }
+
+    // Renders http://localhost:8080/events/create
+    @GetMapping("/create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+
+    // Processes form submitted from http://localhost:8080/events/create
+    @PostMapping("/create")
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
     }
 
 }
