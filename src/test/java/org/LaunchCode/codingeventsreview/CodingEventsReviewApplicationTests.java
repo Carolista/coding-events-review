d8db9456f43643846b47204497f0e85e71bf709e
Carolista <cjones.renaissancewoman@gmail.com>
2023-03-12 11:21:52 -0500

    base project created from start.spring.io - successfully running

---
+++ src/test/java/org/LaunchCode/codingeventsreview/CodingEventsReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.LaunchCode.codingeventsreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEventsReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
