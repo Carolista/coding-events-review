d7c04ed0fb92f3e9431d90583a6c7b927a49b237
Carolista <cjones.renaissancewoman@gmail.com>
2023-03-12 11:33:04 -0500

    added controller and template for home page

---
+++ src/main/java/org/LaunchCode/codingeventsreview/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package org.LaunchCode.codingeventsreview.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping public String displayHomePage() {
+        return "index";
+    }
+}
