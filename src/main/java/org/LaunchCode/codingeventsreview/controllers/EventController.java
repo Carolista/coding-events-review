bfd54127ab263b667b4e4eace3669901656b2aa9
Carolista <cjones.renaissancewoman@gmail.com>
2023-03-12 19:49:40 -0500

    created a data layer and refactored controller accordingly

--- src/main/java/org/LaunchCode/codingeventsreview/controllers/EventController.java
+++ src/main/java/org/LaunchCode/codingeventsreview/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.LaunchCode.codingeventsreview.controllers;
 
+import org.LaunchCode.codingeventsreview.data.EventData;
 import org.LaunchCode.codingeventsreview.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +12,11 @@ import java.util.List;
 @Controller
 @RequestMapping("/events")
 public class EventController {
-    private static final List<Event> events = new ArrayList<>();
 
     // Renders http://localhost:8080/events
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -29,7 +29,7 @@ public class EventController {
     // Processes form submitted from http://localhost:8080/events/create
     @PostMapping("/create")
     public String processCreateEventForm(@RequestParam String eventName, String eventDesc) {
-        events.add(new Event(eventName, eventDesc));
+        EventData.add(new Event(eventName, eventDesc));
         return "redirect:/events";
     }
 
