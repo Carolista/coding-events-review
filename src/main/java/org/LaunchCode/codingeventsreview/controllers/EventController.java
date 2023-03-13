eb2c31bfdb217dce4631c8a11fca165685999801
Carolista <cjones.renaissancewoman@gmail.com>
2023-03-12 19:21:59 -0500

    created Event model and updated controller and view accordingly

--- src/main/java/org/LaunchCode/codingeventsreview/controllers/EventController.java
+++ src/main/java/org/LaunchCode/codingeventsreview/controllers/EventController.java
@@ -1,23 +1,22 @@
 package org.LaunchCode.codingeventsreview.controllers;
 
+import org.LaunchCode.codingeventsreview.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/events")
 public class EventController {
-    private static final Map<String, String> events = new HashMap<>();
+    private static final List<Event> events = new ArrayList<>();
 
     // Renders http://localhost:8080/events
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", events.entrySet());
+        model.addAttribute("events", events);
         return "events/index";
     }
 
@@ -29,8 +28,8 @@ public class EventController {
 
     // Processes form submitted from http://localhost:8080/events/create
     @PostMapping("/create")
-    public String processCreateEventForm(@RequestParam String eventName, String eventDesc) {
-        events.put(eventName, eventDesc);
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.add(new Event(eventName));
         return "redirect:/events";
     }
 
